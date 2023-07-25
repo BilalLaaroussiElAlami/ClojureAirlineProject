f284e4edc27f3f16e5500a94ff366f7ee2632bae
Bilal Lrs <bilal.laaroussi.el.alami@vub.be>
2023-07-25 22:24:47 +0200

    added book-test

--- App/flight_reservation_parallel.clj
+++ App/flight_reservation_parallel.clj
@@ -22,15 +22,15 @@
 ;one update, the overhead is probably bigger than the swap! of an atom
 
 (defn get-available-seats [travel-class]
-  (first travel-class))
-(defn get-price [travel-class]
   (first (rest travel-class)))
+(defn get-price [travel-class]
+  (first travel-class))
 (defn get-taken-seats [travel-class]
   (first (rest (rest travel-class))))
 
 ;I will call flightData  the map that is contained in the flight atom and I will call flight the flight atom
 (defn validate-flight [flightData]
-  (println (str "validating flightdata: " flightData))
+  ;(println (str "validating flightdata: " flightData))
   (let [pricing (flightData :pricing)]
     (every?
      (fn [travel-class]
@@ -49,42 +49,54 @@
 (defn update-flight [flight update-flight-data]
   (loop [oldFlightData @flight]
     (let [newFlightData (update-flight-data oldFlightData)]
-      (println "called update-flight")
-      (println (str "old flight data" oldFlightData))
-      (println (str "newFlightData" newFlightData))
+      ;(println "called update-flight")
+      ;(println (str "old flight data" oldFlightData))
+      ;(println (str "function " update-flight-data))
+      ;(println (str "newFlightData " newFlightData))
       ;if the newdata is not valid (overbooking, negative price, ...) we immediately return false
       ;if we wouldn't do this the automatic validator function in the flight atom would always fail and we would infinitely recur
       ;however this is just an extra safety mechanism because the update-flight-data should not return corrupted data in the first place
       (if (not (validate-flight newFlightData))
         false
         (if (compare-and-set! flight oldFlightData newFlightData)
-          [oldFlightData, newFlightData] ;returning the old an new data so the user can confirm the update really happened
+          [oldFlightData, newFlightData] ;returning the old an new data so the user can confirm if a change really happened
           (recur @flight))))))
 
 
 
 ;I would like to book 5 seats for at most €600 per seat.
-;searches a suitable travel class and returns an updated pricing (will return the same pricing if  a suitable travel class isn't founded)
+;searches a suitable travel class and returns an updated PRICING (will return the same pricing if  a suitable travel class isn't founded)
 (defn take-seats [pricing maxprice seats]
   (let [result
-        (doall  ;might be unnecessary (map on a non-lazy list might not be lazy but idk) TODO find out
-         (map (fn [travel-class]
-                (if (and (< (get-price travel-class) maxprice) (< seats (get-available-seats travel-class)))
-                  [(get-price travel-class) (- (get-available-seats travel-class) seats) (+ (get-taken-seats travel-class) seats)]
-                  travel-class))
-              pricing))]
-    (println "result" result)
-    result))
+          ;might be unnecessary (map on a non-lazy list might not be lazy but idk) TODO find out 
+        (map (fn [travel-class]
+               (if (and (<= (get-price travel-class) maxprice) (<= seats (get-available-seats travel-class)))
+                 [(get-price travel-class) (- (get-available-seats travel-class) seats) (+ (get-taken-seats travel-class) seats)]
+                 travel-class))
+             pricing)]
+    ;(println "result take seats" result)
+    ;map is lazy vec will make a vector out of the lazy sequence
+    (vec result)))
 
 
 ;from, to and id should be already matching with the customer
 ;extra (redundant) safety mechanism: do this check again
 ;returns a boolean that indicates whether the booking succeeded
+
+
+
 (defn book [flight, customer]
   (let [[oldFlightData newFlightData] (update-flight
                                        flight
-                                       (fn [flight-data] (take-seats (flight-data :pricing) (customer :budget) (customer :seats))))]
-    (not (oldFlightData = newFlightData))))
+                                       (fn [flight-data]
+                                         (make-flight-data (flight-data :id)
+                                                           (flight-data :from)
+                                                           (flight-data :to)
+                                                           (flight-data :carrier)
+                                                           (take-seats (flight-data :pricing) (customer :budget) (customer :seats)))))]
+    ;(println (str "oldFlightData" oldFlightData))
+    ;(println (str "newFlightData") newFlightData)
+    (not (= oldFlightData newFlightData))))
 ;As a convenience, = also returns true when used to compare Java collections against each other, or against Clojure’s immutable collections, 
 ;if their contents are equal
 
@@ -287,8 +299,24 @@
 
 (defn book-test []
   (let [F (make-flight 0, "BRU", "ATL", "Delta", [[600 150 0] [650 50 0] [700 50 0]])
-        customer  {:id  1 :from "BRU" :to "ATL" :seats 5 :budget 550}]
-    (println  (str "result booking" (book F customer)))))
+        validcustomer  {:id  1 :from "BRU" :to "ATL" :seats 5 :budget 600}
+        unvalidcustomerA {:id  1 :from "BRU" :to "ATL" :seats 5 :budget 200} ;te weinig budget 
+        unvalidcustomerB {:id  1 :from "BRU" :to "ATL" :seats 160 :budget 200}] ;te veel stoelen
+    (println "BOOKING TEST")
+    (println (str "F before booking: " @F))
+    (println (str "valid customer " validcustomer))
+    (println (str "result booking " (book F validcustomer)))
+    (println (str "F after booking: " @F))
+
+    (println (str "unvalidcustomerA " unvalidcustomerA))
+    (println (str "result booking " (book F unvalidcustomerA)))
+    (println (str "F after booking: " @F))
+
+    (println (str "unvalidcustomerB " unvalidcustomerB))
+    (println (str "result booking " (book F unvalidcustomerB)))
+    (println (str "F after booking: " @F))))
+
+
 
 ;(flight-test)
 (book-test)
